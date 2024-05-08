6dabe0d0ae4804a2b228f7ba329c0abeb9205884
clarity552 <cherrymyintmo552@gmail.com>
2024-05-08 19:08:14 +0630

    first commit

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts/app')
+@section('content')
+    <div class="cotainer m-auto" style="max-width: 800px">
+        @foreach ($articles as $article)
+            <div class="card">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
