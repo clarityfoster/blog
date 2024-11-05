3a01eb37f48df53a389d70155eeae86cb7cf16af
clarity552 <cherrymyintmo552@gmail.com>
2024-11-05 16:26:03 +0630

    update home.blade file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Article; 
+use App\Models\Category;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,30 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $currentUser = auth()->user();
+        $category = Category::all();
+    
+        $data = Article::where(function ($query) use ($currentUser) {
+            $query->where('privacy_id', 1)
+                ->orWhere(function ($query) use ($currentUser) {
+                    if ($currentUser) {
+                        $query->where('privacy_id', 2)
+                                ->whereHas('user.followers', function ($query) use ($currentUser) {
+                                    $query->where('current_user_id', $currentUser->id);
+                                })->orWhere('user_id', $currentUser->id);
+                    }
+                })
+                ->orWhere(function ($query) use ($currentUser) {
+                    if ($currentUser) {
+                        $query->where('privacy_id', 3)
+                                ->where('user_id', $currentUser->id);
+                    }
+                });
+        })->latest()->paginate(4);
+
+        return view('home', [
+            'articles' => $data,
+            'categories' => $category,
+        ]);
     }
 }
